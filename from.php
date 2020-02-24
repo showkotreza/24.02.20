ac554570b72a4f8c7fcf2109609082095016d8ea
showkotreza <57698416+showkotreza@users.noreply.github.com>
2020-02-24 12:36:58 +0600

    Add files via upload

---
+++ from.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html
+	<html>
+<head>
+	<title>form</title>
+</head>
+<body>
+	<FORM>
+		Name: <input type="text" name="name"><br>
+		Email: <input type="text" name="email"><br>
+
+		Gender: <input type="radio" name="Gender" value="Male">Male
+		        <input type="radio" name="Gender" value="Female">Female
+		        <input type="radio" name="Gender" value="Other">Other
+		        <hr>
+		        <br>
+		        password:<input type="password" name="password">
+		        <br>
+		        <input type="submit" name="submit" value="submit">
+                </FORM>
+    </body>
+    </html>
